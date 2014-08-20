1e528a7ac3c75acbf9b75c6bf67c26411328bbe6
Ye Wang <candyye.wang@autodesk.com>
2014-08-20 14:13:21 -0700

    added related work

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,17 @@
 "use strict";
 
+myApp.controller("MaterialCtrl", function($scope, $http, $sce) {
+   $http.get("data/material-project.json").success(function(data) {
+      $scope.papers = data;
+      for (var i = 0; i < $scope.papers.length; i++) {
+         var paper = $scope.papers[i];
+         if (paper.video !== undefined) {
+            paper.video = $sce.trustAsResourceUrl(paper.video);
+         }
+      }
+   });
+});
+
 myApp.controller("SiggraphCtrl", function($scope, $http, $sce) {
    $http.get("data/paper.json").success(function(data) {
       $scope.papers = data;
